ce148381dfe6cfeaff43124bb67d3972090a2cbf
Danny Peck <dannytpeck@gmail.com>
2015-08-22 09:30:47 +0000

    add animation code with placeholder animations

--- js/app.js
+++ js/app.js
@@ -7,10 +7,11 @@ var GRAVITY = 1500;
 var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
-    
+
+    game.load.spritesheet('player', 'assets/slimegirl.png', 42, 64);    
     game.load.image('sky', 'assets/sky.png');
     game.load.image('platform', 'assets/platform.png');
-    game.load.spritesheet('player', 'assets/slimegirl.png', 42, 64);
+    game.load.image('wall', 'assets/wall.png');
 
 }
 
@@ -20,12 +21,7 @@ var jumpTimer = 0;
 var cursors;
 var jumpButton;
 var platforms;
-var onTheGround;
-var jumps;
-var jumping = false;
-var onWall = false;
-var x_pos;
-    
+
 function create() {
 
     // We're going to be using physics, so enable the Arcade Physics system
@@ -44,7 +40,6 @@ function create() {
     game.physics.arcade.enable(player);
 
     // Player physics properties. (Give a little bounce)
-    player.body.bounce.y = 0.2;
     player.body.gravity.y = GRAVITY;
     player.body.maxVelocity.y = 500;
     player.body.collideWorldBounds = true;
@@ -52,6 +47,14 @@ function create() {
     // Our two animations, walking left and right.
     player.animations.add('left', [0, 1, 2, 3, 4, 5, 6, 7], 10, true);
     player.animations.add('right', [8, 9, 10, 11, 12, 13, 14, 15], 10, true);
+    player.animations.add('jump-left', [16], 10, true);
+    player.animations.add('jump-right', [17], 10, true);
+    player.animations.add('fall-left', [18], 10, true);
+    player.animations.add('fall-right', [19], 10, true);
+    player.animations.add('wallstick-left', [20], 10, true);
+    player.animations.add('wallstick-right', [21], 10, true);
+    player.animations.add('duck-left', [22], 10, true);
+    player.animations.add('duck-right', [23], 10, true);
 
     // The platforms group contains the ground and the ledges the player can jump on
     platforms = game.add.group();
@@ -68,10 +71,12 @@ function create() {
     // This stops the ground from falling away when its jumped on
     ground.body.immovable = true;
 
-    // Create two ledges
+    // Create ledges and walls
     var ledge = platforms.create(400, 400, 'platform');
     ledge.body.immovable = true;
-    ledge = platforms.create(-150, 250, 'platform');
+    ledge = platforms.create(-120, 250, 'platform');
+    ledge.body.immovable = true;
+    ledge = platforms.create(800, 0, 'wall');
     ledge.body.immovable = true;
     
     // Our controls
@@ -89,48 +94,81 @@ function update() {
     
     // Reset the player's velocity (movement)
     player.body.velocity.x = 0;
-    
+
     if (cursors.left.isDown) {
         // Move to the left
         player.body.velocity.x = -MOVE_SPEED;
         
-        if (facing != 'left') {
-            player.animations.play('left');
-            facing = 'left';
-        }
+        player.animations.play('left');
+        facing = 'left';
     }
     else if (cursors.right.isDown) {
         // Move to the right
         player.body.velocity.x = MOVE_SPEED;
         
-        if (facing != 'right') {
-            player.animations.play('right');
-            facing = 'right';
-        }
+        player.animations.play('right');
+        facing = 'right';
     }
     else {
-        if (facing != 'idle')
-        {
-            player.animations.stop();
+        player.animations.stop();
 
-            if (facing == 'left')
-            {
-                player.frame = 0;
-            }
-            else
-            {
-                player.frame = 8;
-            }
+        if (facing == 'left') {
+            player.frame = 0;
+        }
+        else {
+            player.frame = 8;
+        }
+    }
+    
+    if (player.body.touching.down && cursors.down.isDown) {
+        //Duck!
+        player.body.velocity.x = 0;
+        
+        if (facing == 'left') {
+            player.animations.play('duck-left');
+        }
+        else if (facing == 'right') {
+            player.animations.play('duck-right');
+        }
+    }
+    
+    if (jumpButton.isDown && game.time.now > jumpTimer && (player.body.touching.down || player.body.touching.left || player.body.touching.right)) {
+        if (player.body.touching.left || player.body.touching.right) {
+            player.body.velocity.y = JUMP_HEIGHT * 0.65;
+        }
+        else {
+            player.body.velocity.y = JUMP_HEIGHT;
+        }
+        jumpTimer = game.time.now + 400;
+    }
 
-            facing = 'idle';
+    // Falling animation    
+    if (player.body.velocity.y < 0) {
+        if (facing == 'left') {
+            player.animations.play('jump-left');
+        }
+        else if (facing == 'right') {
+            player.animations.play('jump-right');
         }
     }
 
-    if (jumpButton.isDown && game.time.now > jumpTimer && (player.body.touching.down || player.body.touching.left || player.body.touching.right)) {
-        player.body.velocity.y = JUMP_HEIGHT;
-        jumpTimer = game.time.now + 400;
-    }    
+    // Falling animation    
+    if (player.body.velocity.y > 0) {
+        if (facing == 'left') {
+            player.animations.play('fall-left');
+        }
+        else if (facing == 'right') {
+            player.animations.play('fall-right');
+        }
+    }
     
+    if (player.body.touching.left) {
+        player.animations.play('wallstick-left');
+    }
+    
+    if (player.body.touching.right) {
+        player.animations.play('wallstick-right');
+    }
 }
 
 function render () {
