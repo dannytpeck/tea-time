1b37b8aa6c7755ccb7566aa1e598f73d33c839c7
Danny Peck <dannytpeck@gmail.com>
2015-08-22 16:52:00 +0000

    add ability to load Tiled maps

--- js/app.js
+++ js/app.js
@@ -8,12 +8,20 @@ var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, '', { preload: preload, c
 
 function preload() {
 
+    game.load.tilemap('level1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('tiles-1', 'assets/tiles-1.png');
+    
     game.load.spritesheet('player', 'assets/slimegirl.png', 42, 64);    
-    game.load.image('sky', 'assets/sky.png');
+    
+    game.load.image('background', 'assets/sky.png');
     game.load.image('platform', 'assets/platform.png');
     game.load.image('wall', 'assets/wall.png');
 
 }
+
+var bg;
+var map;
+var layer;
 
 var player;
 var facing = 'left';
@@ -25,26 +33,39 @@ var isDucking = false;
 
 function create() {
 
-    // We're going to be using physics, so enable the Arcade Physics system
+    // Enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    // Draw background(sky) on the screen
-    //game.add.sprite(0, 0, 'sky');
+    game.stage.backgroundColor = '#000000';
+
+    bg = game.add.tileSprite(0, 0, 800, 600, 'background');
+    bg.fixedToCamera = true;
+
+    map = game.add.tilemap('level1');
     
-    game.add.tileSprite(0, 0, 1600, 600, 'sky');
-    game.world.setBounds(0, 0, 1600, 600);
+    map.addTilesetImage('tiles-1');
+    
+    map.setCollisionByExclusion([ 13, 14, 15, 16, 46, 47, 48, 49, 50, 51 ]);
+    
+    layer = map.createLayer('Tile Layer 1');
+    
+    // Uncomment to see collision tiles
+    layer.debug = true;
+    
+    layer.resizeWorld();
+    
+    game.physics.arcade.gravity.y = GRAVITY;
     
     // The player and its settings
-    player = game.add.sprite(32, game.world.height - 100, 'player');
-    
+    player = game.add.sprite(32, 32, 'player');
     // Enable physics on the player
-    game.physics.arcade.enable(player);
+    game.physics.enable(player, Phaser.Physics.ARCADE);
 
-    // Player physics properties. (Give a little bounce)
-    player.body.gravity.y = GRAVITY;
-    player.body.maxVelocity.y = 500;
+    // Player physics properties
+    player.body.bounce.y = 0.2;
     player.body.collideWorldBounds = true;
-    
+    setPlayerSize();
+
     // Our two animations, walking left and right.
     player.animations.add('left', [0, 1, 2, 3, 4, 5, 6, 7], 10, true);
     player.animations.add('right', [8, 9, 10, 11, 12, 13, 14, 15], 10, true);
@@ -57,29 +78,6 @@ function create() {
     player.animations.add('duck-left', [22], 10, true);
     player.animations.add('duck-right', [23], 10, true);
 
-    // The platforms group contains the ground and the ledges the player can jump on
-    platforms = game.add.group();
-    
-    // Enable physics for any object that is created in this group
-    platforms.enableBody = true;
-    
-    // Create the ground
-    var ground = platforms.create(0, game.world.height - 32, 'platform');
-    
-    // Scale it to fit the width of the game (the original sprite is 400x32)
-    ground.scale.setTo(4, 1); //width multiplier, height multiplier
-    
-    // This stops the ground from falling away when its jumped on
-    ground.body.immovable = true;
-
-    // Create ledges and walls
-    var ledge = platforms.create(400, 400, 'platform');
-    ledge.body.immovable = true;
-    ledge = platforms.create(-120, 250, 'platform');
-    ledge.body.immovable = true;
-    ledge = platforms.create(800, 0, 'wall');
-    ledge.body.immovable = true;
-    
     // Our controls
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -90,8 +88,8 @@ function create() {
 
 function update() {
     
-    // Collide the player with the platforms
-    game.physics.arcade.collide(player, platforms);
+    // Collide the player with the layer
+    game.physics.arcade.collide(player, layer);
     
     // Reset the player's velocity (movement)
     player.body.velocity.x = 0;
@@ -141,7 +139,7 @@ function update() {
         }
     }
     
-    if (player.body.touching.down && cursors.down.isDown) {
+    if (player.body.onFloor() && cursors.down.isDown) {
         //Duck!
         isDucking = true;
     }
@@ -150,7 +148,8 @@ function update() {
         isDucking = false;
     }
     
-    if (jumpButton.isDown && game.time.now > jumpTimer && (player.body.touching.down || player.body.touching.left || player.body.touching.right)) {
+    if (jumpButton.isDown && game.time.now > jumpTimer && (player.body.onFloor() || player.body.onWall())) {
+    //if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer) {
         if (player.body.touching.left || player.body.touching.right) {
             player.body.velocity.y = JUMP_HEIGHT * 0.65;
         }
@@ -160,8 +159,8 @@ function update() {
         jumpTimer = game.time.now + 400;
     }
 
-    // Falling animation    
-    if (player.body.velocity.y < 0) {
+    // Jumping animation
+    if (player.body.velocity.y < -50) {
         if (facing == 'left') {
             player.animations.play('jump-left');
         }
@@ -187,12 +186,26 @@ function update() {
     if (player.body.touching.right) {
         player.animations.play('wallstick-right');
     }
+    
+    // Make smaller if ducking
+    setPlayerSize();
+    
 }
 
 function render () {
 
     // game.debug.text(game.time.physicsElapsed, 32, 32);
-    // game.debug.body(player);
+    game.debug.body(player);
     game.debug.bodyInfo(player, 16, 24);
+    game.debug.text('isDucking:' + isDucking, 16, 128);
 
+}
+
+function setPlayerSize() {
+    if (isDucking) {
+        player.body.setSize(36, 22, 3, 37);
+    }
+    else {
+        player.body.setSize(36, 54, 3, 5);
+    }
 }
