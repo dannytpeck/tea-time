ae1a8f4869024fd8f7faa4c770f43f626e483edc
Danny Peck <dannytpeck@gmail.com>
2015-09-06 21:29:42 +0000

    clean up Preloader

--- js/Preloader.js
+++ js/Preloader.js
@@ -3,8 +3,6 @@ TeaTime.Preloader = function (game) {
 	this.background = null;
 	this.preloadBar = null;
 
-	this.ready = false;
-
 };
 
 TeaTime.Preloader.prototype = {
@@ -36,7 +34,7 @@ TeaTime.Preloader.prototype = {
 
         this.load.image('menu', 'assets/menu window.png');
 
-		// TEST 
+		// test 
     	this.load.image('trans', 'assets/climbwallup_001.png');
         
         // Load the background images
@@ -64,21 +62,8 @@ TeaTime.Preloader.prototype = {
 
 	update: function () {
 
-		//	You don't actually need to do this, but I find it gives a much smoother game experience.
-		//	Basically it will wait for our audio file to be decoded before proceeding to the MainMenu.
-		//	You can jump right into the menu if you want and still play the music, but you'll have a few
-		//	seconds of delay while the mp3 decodes - so if you need your music to be in-sync with your menu
-		//	it's best to wait for it to decode here first, then carry on.
-		
-		//	If you don't have any music in your game then put the game.state.start line into the create function and delete
-		//	the update function completely.
-		
-		if (this.ready == false)
-		{
-			this.ready = true;
-			//this.state.start('MainMenu');
-			this.state.start('Cave');
-		}
+		//this.state.start('MainMenu');
+		this.state.start('Cave');
 
 	}
 
